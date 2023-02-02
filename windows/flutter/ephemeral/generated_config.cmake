4a5096f3d095253808ee1e62f05209ccd01abf71
Evilking009 <muzaffarhassan64@gmail.com>
2023-02-03 04:24:57 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "PROJECT_DIR=C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application"
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application"
+  "FLUTTER_TARGET=C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Maxx\\Documents\\Flutter Projects\\map_application\\.dart_tool\\package_config.json"
+)
